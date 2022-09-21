879c29bf3210f7cc6b4b38ed26825fcdcf76e40a
Theodore_Lopez <theodore.lopez@lis-lab.fr>
2022-09-21 16:53:06 +0200

    test_transition

--- test.py
+++ test.py
@@ -1,2 +1,22 @@
-def test():
-    assert(False)
+def test_tauto():
+    assert(True)
+
+def test_load_file():
+    jeu = charger_fichier(example_petit.vie)
+    jeu[0][0] = "."
+    jeu[0][1] = "*"
+    jeu[1][0] = "*"
+    jeu[1][1] = "*"
+
+def test_transition():
+    jeu = charger_fichier(example_petit.vie)
+    jeu2 = transition(jeu)
+    jeu2[0][0] = "*"
+    jeu2[0][1] = "*"
+    jeu2[1][0] = "*"
+    jeu2[1][1] = "*"
+    jeu3 = transition(jeu2)
+    jeu3[0][0] = "."
+    jeu3[0][1] = "."
+    jeu3[1][0] = "."
+    jeu3[1][1] = "."
