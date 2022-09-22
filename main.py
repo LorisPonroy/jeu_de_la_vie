812a179528125c2c4ee4c28ea0d2e40b8a29fb74
Theodore_Lopez <theodore.lopez@lis-lab.fr>
2022-09-22 21:22:30 +0200

    Test et transition du jeu de la vie

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print("Hello World")
+from typing import List
+
+def charger_fichier(nom_fichier:str) -> List[List[bool]]:
+    """Charge le fichier et renvoie le jeu correspondant"""
+    pass
+    #Vérifier que toutes les lignes font la bonne taille
+
+def compte_voisins(jeu : List[List[bool]], x, y) -> int:
+    """Donne le nombre de voisins de la cellule x,y dans le jeu"""
+    nb_voisins = 0
+    for i in range(-1,2):
+        for j in range(-1,2):
+            if 0 <= x+i < len(jeu) and 0 <= y+j < len(jeu[0]):
+                if (i!=0 or j!= 0) and jeu[x+i][y+j]:
+                    nb_voisins += 1
+    return nb_voisins
+
+def transition(jeu : List[List[bool]]) -> List[List[bool]]:
+    """Renvoie le jeu après une transition"""
+    jeu_apres = []
+    for i in range(len(jeu)):
+        jeu_apres.append([])
+        for j in range(len(jeu[0])):
+            n = compte_voisins(jeu,i,j)
+            if n == 2:
+                cell = jeu[i][j]
+            elif n == 3:
+                cell = True 
+            else:
+                cell = False
+            jeu_apres[i].append(cell)
+    return jeu_apres
--- test.py
+++ test.py
@@ -1,22 +1,36 @@
+from typing import List
+from main import *
+
 def test_tauto():
     assert(True)
 
+example_jeu1 = [[False,True],[True,True]]
+example_jeu2 = [[True,False],[True,False]]
+
 def test_load_file():
     jeu = charger_fichier(example_petit.vie)
-    jeu[0][0] = "."
-    jeu[0][1] = "*"
-    jeu[1][0] = "*"
-    jeu[1][1] = "*"
+    jeu[0][0] = False
+    jeu[0][1] = True
+    jeu[1][0] = True
+    jeu[1][1] = True
+
+def test_compte_voisins():
+    compte_voisins(example_jeu1,0,0) == 3
+    compte_voisins(example_jeu1,0,1) == 2
+    compte_voisins(example_jeu1,1,0) == 2
+    compte_voisins(example_jeu1,1,1) == 2
+    compte_voisins(example_jeu1,0,0) == 1
+    compte_voisins(example_jeu1,0,1) == 2
+    compte_voisins(example_jeu1,1,0) == 1
+    compte_voisins(example_jeu1,1,1) == 2
 
 def test_transition():
-    jeu = charger_fichier(example_petit.vie)
-    jeu2 = transition(jeu)
-    jeu2[0][0] = "*"
-    jeu2[0][1] = "*"
-    jeu2[1][0] = "*"
-    jeu2[1][1] = "*"
+    jeu2 = transition(example_jeu1)
+    jeu2[0][0] = True
+    jeu2[0][1] = True
+    jeu2[1][0] = True
+    jeu2[1][1] = True
     jeu3 = transition(jeu2)
-    jeu3[0][0] = "."
-    jeu3[0][1] = "."
-    jeu3[1][0] = "."
-    jeu3[1][1] = "."
+    jeu3 == jeu2
+
+    
